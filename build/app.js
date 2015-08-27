d84d1702a4d6c44fa67bb8ea2c18f95c320bbbf5
Souren Papazian <souren@papazian.info>
2015-08-27 14:11:32 -0700

    peers are now sorted by uptime on dashboard

--- build/app.js
+++ build/app.js
@@ -58410,12 +58410,20 @@ app.controller('PeersCtrl', ['$scope', function ($scope) {
   $scope.status = "Loading Peers..."
 
   Peers.fetch().then(function(peers) {
+    // Sort peers by uptime
+    peers.sort(function(a, b){
+      var uptime_a = a.uptime ? a.uptime : -1;
+      var uptime_b = b.uptime ? b.uptime : -1;
+      if(uptime_a < uptime_b) return 1;
+      if(uptime_a > uptime_b) return -1;
+      return 0;
+    });
+
     $scope.loading = false
     $scope.peers = peers
     $scope.$apply()
   })
 }])
-
 
 },{"../app":26,"../services/Peers":33}],30:[function(require,module,exports){
 (function (global){
