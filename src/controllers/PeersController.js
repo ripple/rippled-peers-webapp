973b6cce85bdcc562acd8cca78e1cc230b04cda8
Souren Papazian <souren@papazian.info>
2015-08-27 14:20:16 -0700

    moved peer sorting into the Peers service

--- src/controllers/PeersController.js
+++ src/controllers/PeersController.js
@@ -7,17 +7,8 @@ app.controller('PeersCtrl', ['$scope', function ($scope) {
   $scope.status = "Loading Peers..."
 
   Peers.fetch().then(function(peers) {
-    // Sort peers by uptime
-    peers.sort(function(a, b){
-      var uptime_a = a.uptime ? a.uptime : -1;
-      var uptime_b = b.uptime ? b.uptime : -1;
-      if(uptime_a < uptime_b) return 1;
-      if(uptime_a > uptime_b) return -1;
-      return 0;
-    });
-
     $scope.loading = false
-    $scope.peers = peers
+    $scope.peers = Peers.sortByUptime(peers);
     $scope.$apply()
   })
 }])
--- src/services/Peers.js
+++ src/services/Peers.js
@@ -16,6 +16,16 @@ module.exports = {
           }
         })
     })
+  },
+
+  sortByUptime: function(peers) {
+    peers.sort(function(a, b){
+      var uptime_a = a.uptime ? a.uptime : -1;
+      var uptime_b = b.uptime ? b.uptime : -1;
+      if(uptime_a < uptime_b) return 1;
+      if(uptime_a > uptime_b) return -1;
+      return 0;
+    });
+    return peers;
   }
 }
-
